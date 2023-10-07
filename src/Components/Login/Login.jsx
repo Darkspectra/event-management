38ea12a17d29550523103c7648b675e559ec08db
Darkspectra <ftanvirnovo@gmail.com>
2023-10-07 19:37:02 +0600

    Fixed frontend for login and register

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -8,13 +8,13 @@ import { FaGoogle } from "react-icons/fa";
 
 
 const Login = () => {
-    const {signIn} = useContext(AuthContext)
+    const { signIn } = useContext(AuthContext)
 
     const location = useLocation();
     const navigate = useNavigate();
     const [loginError, setLoginError] = useState('')
 
-    const handleLogin = e =>{
+    const handleLogin = e => {
         e.preventDefault()
         const form = new FormData(e.currentTarget)
         const email = form.get('email')
@@ -28,44 +28,47 @@ const Login = () => {
             .catch(error => {
                 console.error(error);
                 setLoginError(error.message);
-            })}
+            })
+    }
 
     const auth = getAuth(app);
     const provider = new GoogleAuthProvider();
 
     const handleGoogleSignIn = () => {
         signInWithPopup(auth, provider)
-        .then(result => {
-            const user = result.user;
-            navigate(location?.state ? location.state : '/')
-            console.log(user);
-        })
-        .catch(error=>{
-            console.log(error);
-        })
+            .then(result => {
+                const user = result.user;
+                navigate(location?.state ? location.state : '/')
+                console.log(user);
+            })
+            .catch(error => {
+                console.log(error);
+            })
     }
 
     return (
         <div>
-            <h2 className="text-4xl italic font-bold text-red-400 text-center">~Espousal~</h2>
-            <p className="text-2xl text-center">A Wedding Management</p>
+            <div className="bg-green-300 rounded-xl">
+                <h2 className="text-4xl italic font-bold text-red-400 text-center">~Espousal~</h2>
+                <p className="text-2xl text-center">A Wedding Management</p>
+            </div>
             <div>
                 <h2 className="text-3xl my-10 text-center">Please Login</h2>
                 <form onSubmit={handleLogin} className="md: w-3/4 lg:w-1/2 mx-auto">
                     <div className="form-control">
                         <label className="label">
-                            <span className="label-text">Email</span>
+                            <span className="label-text text-3xl">Email</span>
                         </label>
                         <input type="email" placeholder="email" name="email" className="input input-bordered" required />
                     </div>
                     <div className="form-control">
                         <label className="label">
-                            <span className="label-text">Password</span>
+                            <span className="label-text text-3xl">Password</span>
                         </label>
                         <input type="password" placeholder="password" name="password" className="input input-bordered" required />
                     </div>
                     <div className="form-control mt-6">
-                        <button className="btn btn-primary">Login</button>
+                        <button className="btn btn-primary text-3xl">Login</button>
                     </div>
                 </form>
                 <p className="text-center mt-4">Do not have an account <Link to="/register" className="text-blue-800 font-bold">Register</Link></p>
--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -45,31 +45,33 @@ const Register = () => {
     }
     return (
         <div>
-            <h2 className="text-4xl italic font-bold text-red-400 text-center">~Espousal~</h2>
-            <p className="text-2xl text-center">A Wedding Management</p>
+            <div className="bg-green-300 rounded-xl">
+                <h2 className="text-4xl italic font-bold text-red-400 text-center">~Espousal~</h2>
+                <p className="text-2xl text-center">A Wedding Management</p>
+            </div>
             <div>
                 <h2 className="text-3xl my-10 text-center">Please Register</h2>
                 <form onSubmit={handleRegister} className="md: w-3/4 lg:w-1/2 mx-auto">
                     <div className="form-control">
                         <label className="label">
-                            <span className="label-text">Name</span>
+                            <span className="label-text text-3xl">Name</span>
                         </label>
                         <input type="text" placeholder="Name" name="name" className="input input-bordered" required />
                     </div>
                     <div className="form-control">
                         <label className="label">
-                            <span className="label-text">Email</span>
+                            <span className="label-text text-3xl">Email</span>
                         </label>
                         <input type="email" placeholder="email" name="email" className="input input-bordered" required />
                     </div>
                     <div className="form-control">
                         <label className="label">
-                            <span className="label-text">Password</span>
+                            <span className="label-text text-3xl">Password</span>
                         </label>
                         <input type="password" placeholder="password" name="password" className="input input-bordered" required />
                     </div>
                     <div className="form-control mt-6">
-                        <button className="btn btn-primary">Register</button>
+                        <button className="btn btn-primary text-3xl">Register</button>
                     </div>
                 </form>
                 <p className="text-center mt-4">Already have an account <Link to="/login" className="text-blue-800 font-bold">Login</Link></p>
